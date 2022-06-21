3392aa4e9b7d6901a20cd356d38e712140b30466
hitesh-ahuja-sc <49285184+hitesh-ahuja-sc@users.noreply.github.com>
2022-06-21 21:14:32 +0530

    Update assets_audio_player.dart

--- lib/src/assets_audio_player.dart
+++ lib/src/assets_audio_player.dart
@@ -520,7 +520,7 @@ class AssetsAudioPlayer {
     _playerEditor = null;
 
     if (_lifecycleObserver != null) {
-      WidgetsBinding.instance.removeObserver(_lifecycleObserver!);
+      WidgetsBinding?.instance?.removeObserver(_lifecycleObserver!);
       _lifecycleObserver = null;
     }
   }
